6dc7dfc34519917a2929818c2d1e6af073b80c9d
Mikayla M <54966992+verdetea22@users.noreply.github.com>
2022-12-13 21:20:12 -0500

    Create MTTDtest.py

---
+++ MTTDtest.py
@@ -0,0 +1,42 @@
+"""
+Write test cases to test each function you implemented
+Please provide comments to each test case you create to explain what the test case is testing. Please report the coverage of your test cases.
+ Output screenshot for coverage should indicate >80% coverage.
+"""
+
+"""
+Testing Function 1: Get the information in MRZ as two strings 
+Input: Scanned MRZ lines (string) 
+Output: Validation (boolean)    
+"""
+def testScanMRZ():
+      
+      pass
+  
+  
+"""
+Testing Function 2: Decode the MRZ's two strings and cacluate check digits   
+Input: Scanned MRZ lines (string) 
+Output: Validation (boolean) 
+"""
+def testDecodeMRZ():
+      
+      return()
+  
+"""
+Testing Function 3: Encode travel information fields queried from a database into the two strings for the MRZ
+Input: Decoded info (string), Encoded info (string) 
+Output: Validation (boolean) 
+"""
+def testEncodeMRZ():
+      
+      return()
+  
+"""
+Function 4: report a mismatch between certain information fields and the check digit. The system shall report where error occured relative to check digit
+Input: Scanned MRZ lines (string), Mismatched info (string) 
+Output:  Validation (boolean) 
+"""
+def testErrorMRZ():
+      
+      return()  
